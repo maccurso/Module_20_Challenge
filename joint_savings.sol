1954949c15510ce89c724d5206c59d9b7b6f3626
maccurso <108839273+maccurso@users.noreply.github.com>
2023-04-25 22:24:28 -0400

    final code and screenshots

--- joint_savings.sol
+++ joint_savings.sol
@@ -82,8 +82,8 @@ contract JointSavings {
     function setAccounts(address payable account1, address payable account2) public{
 
         // Set the values of `accountOne` and `accountTwo` to `account1` and `account2` respectively.
-        account1 = accountOne;
-        account2 = accountTwo;
+        accountOne = account1;
+        accountTwo = account2;
     }
 
     /*
